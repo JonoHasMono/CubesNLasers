d0c037f058e1731c926bba9dba5735c896fb3846
JonoHasMono <fry22331@lcsd2kids.org>
2021-08-26 13:43:06 -0600

    created the css styles for the menu

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const bodyVar = document.createElement("div");
 bodyVar.setAttribute("id", "game");
 document.body.appendChild(bodyVar);
+
+let selectedCube = 0;
+
+function createMenu() {
+    let menu = document.createElement("div");
+    menu.setAttribute("id", "menu");
+    menu.setAttribute("class", "menu");
+    menu.innerHTML ="Menu";
+    bodyVar.appendChild(menu)
+}
+
+function start() {
+    createMenu();
+
+}
+
+start();
